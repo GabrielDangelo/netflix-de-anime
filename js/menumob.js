e55307b4b6ce68056da8d2a996eb1ccf04ddbd32
GabrielDangelo <gabrieldangelo13@gmail.com>
2021-09-28 10:42:59 -0300

    clone netflix

---
+++ js/menumob.js
@@ -0,0 +1,10 @@
+const btnmobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+    if(event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+btnmobile.addEventListener('click', toggleMenu);
+btnmobile.addEventListener('touchstart', toggleMenu);
---
+++ js/modals.js
@@ -0,0 +1,23 @@
+function abrirInfo(){
+    let modal = document.querySelector(".mais-info");
+
+    modal.style.display = 'block';
+}
+
+function fecharInfo(){
+    let modal = document.querySelector(".mais-info");
+
+    modal.style.display = 'none';
+}
+
+
+function abrirTrailer(){
+    let modal = document.querySelector(".trailer");
+
+    modal.style.display = 'block';
+}
+function fecharTrailer(){
+    let modal = document.querySelector(".trailer");
+
+    modal.style.display = "none";
+}
